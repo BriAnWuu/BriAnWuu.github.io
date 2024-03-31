eb6063d862da191aea1d75a81c5466d681087652
brianwu <fslikeyou1033@yahoo.com>
2024-03-31 18:14:40 -0400

    init commit

---
+++ assets/js/galleryitems.js
@@ -0,0 +1,44 @@
+const galleryItems = [
+    {
+        fullLink: 'images/fulls/01.jpg',
+        thumbLink: 'images/thumbs/01.jpg',
+        repoLink: 'https://github.com/ajlkn/jquery.poptrox',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+    {
+        fullLink: 'images/fulls/02.jpg',
+        thumbLink: 'images/thumbs/02.jpg',
+        repoLink: '#',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+    {
+        fullLink: 'images/fulls/03.jpg',
+        thumbLink: 'images/thumbs/03.jpg',
+        repoLink: '#',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+    {
+        fullLink: 'images/fulls/04.jpg',
+        thumbLink: 'images/thumbs/04.jpg',
+        repoLink: '#',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+    {
+        fullLink: 'images/fulls/05.jpg',
+        thumbLink: 'images/thumbs/05.jpg',
+        repoLink: '#',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+    {
+        fullLink: 'images/fulls/06.jpg',
+        thumbLink: 'images/thumbs/06.jpg',
+        repoLink: '#',
+        header: 'Hello',
+        paragraph: 'you good.'
+    },
+];
