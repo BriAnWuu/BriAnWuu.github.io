dc509f24021bdb66e4356ab64d3688fec1329b28
brianwu <fslikeyou1033@yahoo.com>
2024-04-08 16:12:27 -0400

    gallery items

--- assets/js/galleryitems.js
+++ assets/js/galleryitems.js
@@ -4,41 +4,41 @@ const galleryItems = [
         thumbLink: 'images/thumbs/01.jpg',
         repoLink: 'https://public.tableau.com/app/profile/brian.w1342/viz/CyclisticNYCBikeShare/Story',
         header: 'Cyclistic NYC Bike Share',
-        paragraph: 'you good.'
+        paragraph: 'description'
     },
     {
         fullLink: 'images/fulls/02.jpg',
         thumbLink: 'images/thumbs/02.jpg',
-        repoLink: '#',
-        header: 'Hello',
-        paragraph: 'you good.'
+        repoLink: 'https://github.com/BriAnWuu/UIUC-FIN514',
+        header: 'Complex Securities Valuation & Research',
+        paragraph: 'description'
     },
     {
         fullLink: 'images/fulls/03.jpg',
         thumbLink: 'images/thumbs/03.jpg',
-        repoLink: '#',
-        header: 'Hello',
-        paragraph: 'you good.'
+        repoLink: 'https://github.com/BriAnWuu/UIUC-FIN510',
+        header: 'Chicago Property Value Assessment',
+        paragraph: 'description'
     },
     {
         fullLink: 'images/fulls/04.jpg',
         thumbLink: 'images/thumbs/04.jpg',
         repoLink: '#',
-        header: 'Hello',
-        paragraph: 'you good.'
+        header: 'Financial Risk Management Project',
+        paragraph: 'description'
     },
     {
         fullLink: 'images/fulls/05.jpg',
         thumbLink: 'images/thumbs/05.jpg',
-        repoLink: '#',
-        header: 'Hello',
-        paragraph: 'you good.'
+        repoLink: 'https://public.tableau.com/app/profile/brian.w1342/viz/MetroInterstateTrafficVolume_17099174121610/Dashboard1',
+        header: 'Minnesota Interstate Traffic Volume',
+        paragraph: 'description'
     },
     {
         fullLink: 'images/fulls/06.jpg',
         thumbLink: 'images/thumbs/06.jpg',
-        repoLink: '#',
-        header: 'Hello',
-        paragraph: 'you good.'
-    },
+        repoLink: 'https://github.com/BriAnWuu/Sentiment_Analysis',
+        header: 'Sentiment Analysis on Financial News',
+        paragraph: 'description'
+    }, 
 ];
